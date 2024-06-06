d038e6a95e021bbdb81d7be3316705f7825edc5d
Visal VR <visal.v@thinkpalm.com>
2024-06-06 14:50:25 +0530

    ADD: Added button and style.

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,7 @@
+export default function AddTopic(){
+    return (
+        <form className="flex flex-col gap-3">
+            <input className="border border-slate-500 px-8 py-2" type="text" placeHolder="Topic Title"></input>
+        </form>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="max-w-3xl mx-auto p-4">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
