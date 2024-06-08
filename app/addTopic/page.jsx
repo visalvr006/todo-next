865556f484de29c3f58c5b6b530895a8b6f8324d
unknown <visalvr006@gmail.com>
2024-06-08 19:33:07 +0530

    Added service.

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -2,6 +2,8 @@ export default function AddTopic(){
     return (
         <form className="flex flex-col gap-3">
             <input className="border border-slate-500 px-8 py-2" type="text" placeHolder="Topic Title"></input>
+            <input className="border border-slate-500 px-8 py-2" type="text" placeHolder="Topic Description"></input>
+        <button className="bg-green-600 text-white font-bold text-white py-3 px-6 w-fit">Add Topic</button>
         </form>
     )
 }
